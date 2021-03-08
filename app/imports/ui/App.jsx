7be81f25feff1fad20b9c25c736ce1c918d1e847
yhanessaanne <77658704+yhanessaanne@users.noreply.github.com>
2021-03-07 16:36:21 -1000

    Section 6

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
+import React, { useState } from 'react';
 
 const toggleChecked = ({ _id, isChecked }) => {
   TasksCollection.update(_id, {
@@ -15,22 +15,45 @@ const toggleChecked = ({ _id, isChecked }) => {
 const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
 export const App = () => {
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
   const tasks = useTracker(() =>
-      TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+      TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+        sort: { createdAt: -1 },
+      }).fetch()
   );
+
+  const pendingTasksCount = useTracker(() =>
+      TasksCollection.find(hideCompletedFilter).count()
+  );
+
+  const pendingTasksTitle = `${
+      pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
 
   return (
       <div className="app">
         <header>
           <div className="app-bar">
             <div className="app-header">
-              <h1>📝️ To Do List</h1>
+              <h1>
+                📝️ To Do List
+                {pendingTasksTitle}
+              </h1>
             </div>
           </div>
         </header>
 
         <div className="main">
           <TaskForm />
+
+          <div className="filter">
+            <button onClick={() => setHideCompleted(!hideCompleted)}>
+              {hideCompleted ? 'Show All' : 'Hide Completed'}
+            </button>
+          </div>
 
           <ul className="tasks">
             {tasks.map(task => (
